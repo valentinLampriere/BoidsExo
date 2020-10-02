13b9d65264f50fe299aed6bbcea28d39896d5fc1
valentinLampriere <valentin.lampriere1@gmail.com>
2020-10-02 19:47:15 +0200

    Refractor code : Boid manage themselves

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -6,16 +6,151 @@ using UnityEngine.VFX;
 
 public class Boid : MonoBehaviour {
     public Collider2D boidCollider;
+    private BoidManager manager;
 
     public void Start() {
         boidCollider = GetComponent<Collider2D>();
+    }
+    public void Init(BoidManager bm) {
+        manager = bm;
     }
 
     public Vector3 calcVelocity(Vector3 direction, float speed) {
         return direction * Time.deltaTime * speed;
     }
 
-    public void Move(Vector3 velocity) {
+    Vector3 MoveCohesion(List<Transform> closeBoids) {
+        Vector3 cohesionVelocity = Vector3.zero;
+        if (closeBoids.Count == 0) return cohesionVelocity;
+        foreach (Transform _boid in closeBoids) {
+            cohesionVelocity += _boid.position;
+        }
+        cohesionVelocity /= closeBoids.Count;
+        cohesionVelocity -= transform.position;
+        return cohesionVelocity * manager.cohesionStrength;
+    }
+    Vector3 MoveSeparation(List<Transform> closeBoids) {
+        Vector3 separationVelocity = Vector3.zero;
+        int numClose = 0;
+        if (closeBoids.Count == 0) return separationVelocity;
+        foreach (Transform _boid in closeBoids) {
+            float distance = Vector3.Distance(transform.position, _boid.position);
+            if (distance <= manager.boidMinimumDistance) {
+                separationVelocity += transform.position - _boid.position;
+                numClose++;
+            }
+        }
+        if (numClose > 0) {
+            separationVelocity /= numClose;
+        }
+        return separationVelocity * manager.separationStrength * 2f;
+    }
+    Vector3 MoveAlignement(List<Transform> closeBoids) {
+        Vector3 alignementVelocity = Vector3.zero;
+        if (closeBoids.Count == 0) return alignementVelocity;
+        foreach (Transform _boid in closeBoids) {
+            alignementVelocity += _boid.up;
+        }
+        alignementVelocity /= closeBoids.Count;
+
+        return alignementVelocity * manager.alignementStrength;
+    }
+
+    Vector3 MergeVelocities(Vector3 cohesionVelocity, Vector3 alignementVelocity, Vector3 separationVelocity) {
+        Vector3 direction = (transform.up * 8f + cohesionVelocity + alignementVelocity + separationVelocity);
+        return direction;
+    }
+
+    Vector3 ManageVelocity(Vector3 moveDirection, List<Transform> closeObjects) {
+        Vector3 newVelocity = moveDirection;
+
+        newVelocity += AvoidObstacles(closeObjects);
+        newVelocity = calcVelocity(newVelocity, manager.boidSpeed);
+        newVelocity = ManageFieldLimit(newVelocity);
+        return newVelocity;
+    }
+
+    Vector3 AvoidObstacles(List<Transform> closeObjects) {
+        Vector3 freeDirection;
+        int i = 0;
+        bool checkLeft = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, manager.boidVisionDistance, LayerMask.GetMask("Obstacles"));
+
+        if (closeObjects.Count == 0 || hit.collider == null)
+            return Vector3.zero;
+
+        while (IsObstacleInDirection(transform.position, transform.up, i, checkLeft, out freeDirection)) {
+            if (i >= 8)
+                return -transform.up;
+            if (checkLeft) i++;
+            checkLeft = !checkLeft;
+        }
+
+        return freeDirection;
+    }
+    Vector3 ManageFieldLimit(Vector3 velocity) {
+        if (!manager.goThroughtWalls) {
+            if (velocity.x < 0 && transform.position.x + velocity.x < -manager.widthOfArea / 2 || velocity.x > 0 && transform.position.x + velocity.x > manager.widthOfArea / 2) {
+                velocity.x = -velocity.x;
+            }
+            if (velocity.y < 0 && transform.position.y + velocity.y < -manager.heightOfArea / 2 || velocity.y > 0 && transform.position.y + velocity.y > manager.heightOfArea / 2) {
+                velocity.y = -velocity.y;
+            }
+        }
+        else {
+            if (velocity.x < 0 && transform.position.x + velocity.x < -manager.widthOfArea / 2) {
+                transform.position = new Vector3(manager.widthOfArea / 2, transform.position.y);
+            }
+            else if (velocity.x > 0 && transform.position.x + velocity.x > manager.widthOfArea / 2) {
+                transform.position = new Vector3(-manager.widthOfArea / 2, transform.position.y);
+            }
+            if (velocity.y < 0 && transform.position.y + velocity.y < -manager.heightOfArea / 2) {
+                transform.position = new Vector3(transform.position.x, manager.heightOfArea / 2);
+            }
+            else if (velocity.y > 0 && transform.position.y + velocity.y > manager.heightOfArea / 2) {
+                transform.position = new Vector3(transform.position.x, -manager.heightOfArea / 2);
+            }
+        }
+        return velocity;
+    }
+    bool IsObstacleInDirection(Vector3 origin, Vector3 direction, int offset, bool checkLeft, out Vector3 freeDirection) {
+        float cos = Mathf.Cos(Mathf.PI / 8f * offset);
+        float sin = Mathf.Sin(Mathf.PI / 8f * offset);
+        if (checkLeft) {
+            cos = Mathf.Cos(-Mathf.PI / 8f * offset);
+            sin = Mathf.Sin(-Mathf.PI / 8f * offset);
+        }
+
+        float dX = direction.x * cos - direction.y * sin;
+        float dY = direction.x * sin + direction.y * cos;
+
+        Vector3 rayDirection = new Vector3(dX, dY);
+        RaycastHit2D hit = Physics2D.Raycast(origin, rayDirection, manager.boidVisionDistance, LayerMask.GetMask("Obstacles"));
+        if (hit.collider != null) {
+            Debug.DrawLine(origin, origin + direction, Color.green);
+            freeDirection = Vector3.zero;
+            return true;
+        }
+        else {
+            Debug.DrawLine(origin, origin + direction, Color.red);
+            freeDirection = rayDirection;
+            return false;
+        }
+    }
+
+    public void Update() {
+        List<Transform> closeBoids;
+        List<Transform> closeObjects;
+        manager.GetCloseObjects(this, out closeBoids, out closeObjects);
+
+        Vector3 cohesionVelocity = MoveCohesion(closeBoids);
+        Vector3 alignementVelocity = MoveAlignement(closeBoids);
+        Vector3 separationVelocity = MoveSeparation(closeBoids);
+
+        Vector3 moveDirection = MergeVelocities(cohesionVelocity, alignementVelocity, separationVelocity);
+        Vector3 velocity = ManageVelocity(moveDirection, closeObjects);
+
         transform.up = velocity;
         transform.position += velocity;
     }
--- Assets/BoidManager.cs
+++ Assets/BoidManager.cs
@@ -36,6 +36,8 @@ public class BoidManager : MonoBehaviour {
     List<Boid> allBoids = new List<Boid>();
     List<Collider2D> allObstacles = new List<Collider2D>();
 
+    private Boid activeBoid;
+
     // Start is called before the first frame update
     void Start() {
         for (int i = 0; i < amountOfBoids; i++) {
@@ -46,51 +48,13 @@ public class BoidManager : MonoBehaviour {
             objBoid.name = "Boid " + i;
             if (b != null) {
                 allBoids.Add(b);
+                b.Init(this);
             }
         }
-    }
-
-    Vector3 MoveCohesion(Boid boid, List<Transform> closeBoids) {
-        Vector3 cohesionVelocity = Vector3.zero;
-        if (closeBoids.Count == 0) return cohesionVelocity;
-        foreach (Transform _boid in closeBoids) {
-            cohesionVelocity += _boid.position;
-        }
-        cohesionVelocity /= closeBoids.Count;
-        cohesionVelocity -= boid.transform.position;
-        return cohesionVelocity * cohesionStrength;
-    }
-
-    Vector3 MoveSeparation(Boid boid, List<Transform> closeBoids) {
-        Vector3 separationVelocity = Vector3.zero;
-        int numClose = 0;
-        if (closeBoids.Count == 0) return separationVelocity;
-        foreach (Transform _boid in closeBoids) {
-            float distance = Vector3.Distance(boid.transform.position, _boid.position);
-            if (distance <= boidMinimumDistance) {
-                separationVelocity += boid.transform.position - _boid.position;
-                numClose++;
-            }
-        }
-        if (numClose > 0) {
-            separationVelocity /= numClose;
-        }
-        return separationVelocity * separationStrength * 2f;
-    }
-
-    Vector3 MoveAlignement(Boid boid, List<Transform> closeBoids) {
-        Vector3 alignementVelocity = Vector3.zero;
-        if (closeBoids.Count == 0) return alignementVelocity;
-        foreach (Transform _boid in closeBoids) {
-            alignementVelocity += _boid.up;
-        }
-        alignementVelocity /= closeBoids.Count;
-
-        return alignementVelocity * alignementStrength;
     }
 
     void Update() {
-        foreach(Boid boid in allBoids) {
+        /*foreach(Boid boid in allBoids) {
             List<Transform> closeBoids;
             List<Transform> closeObjects;
             GetCloseObjects(boid, out closeBoids, out closeObjects);
@@ -103,7 +67,7 @@ public class BoidManager : MonoBehaviour {
             Vector3 moveDirection = MergeVelocities(boid.transform.up, cohesionVelocity, alignementVelocity, separationVelocity);
 
             boid.Move(ManageVelocity(boid, moveDirection, closeObjects));
-        }
+        }*/
 
         // Spawn obstacles on click
         if (Input.GetButtonDown("Fire1")) {
@@ -115,90 +79,20 @@ public class BoidManager : MonoBehaviour {
             o.tag = "Obstacle";
             allObstacles.Add(o.GetComponent<Collider2D>());
         }
-    }
 
-    Vector3 MergeVelocities(Vector3 boidDirection, Vector3 cohesionVelocity, Vector3 alignementVelocity, Vector3 separationVelocity) {
-        Vector3 direction = (boidDirection * 8f + cohesionVelocity + alignementVelocity + separationVelocity);
-        return direction;
-    }
-
-    Vector3 ManageVelocity(Boid boid, Vector3 moveDirection, List<Transform> closeObjects) {
-        Vector3 newVelocity = moveDirection;
-
-        newVelocity += AvoidObstacles(boid.transform, closeObjects);
-        newVelocity = boid.calcVelocity(newVelocity, boidSpeed);
-        newVelocity = ManageFieldLimit(boid, newVelocity);
-        //Debug.Log(AvoidObstacles(boid.transform, closeObjects));
-        return newVelocity;
-    }    
-
-    Vector3 AvoidObstacles(Transform boidTransform, List<Transform> closeObjects) {
-        Vector3 freeDirection;
-        int i = 0;
-        bool checkLeft = false;
-
-        RaycastHit2D hit = Physics2D.Raycast(boidTransform.position, boidTransform.up, boidVisionDistance, LayerMask.GetMask("Obstacles"));
-
-        if (closeObjects.Count == 0 || hit.collider == null)
-            return Vector3.zero;
-
-        while (IsObstacleInDirection(boidTransform.position, boidTransform.up, i, checkLeft, out freeDirection)) {
-            if (i >= 8)
-                return -boidTransform.up;
-            if (checkLeft) i++;
-            checkLeft = !checkLeft;
-        }
-
-        return freeDirection;
-    }
-    bool IsObstacleInDirection(Vector3 origin, Vector3 direction, int offset, bool checkLeft, out Vector3 freeDirection) {
-        float cos = Mathf.Cos(Mathf.PI / 8f * offset);
-        float sin = Mathf.Sin(Mathf.PI / 8f * offset);
-        if (checkLeft) {
-            cos = Mathf.Cos(-Mathf.PI / 8f * offset);
-            sin = Mathf.Sin(-Mathf.PI / 8f * offset);
-        }
-
-        float dX = direction.x * cos - direction.y * sin;
-        float dY = direction.x * sin + direction.y * cos;
-
-        Vector3 rayDirection = new Vector3(dX, dY);
-        RaycastHit2D hit = Physics2D.Raycast(origin, rayDirection, boidVisionDistance, LayerMask.GetMask("Obstacles"));
-        if (hit.collider != null) {
-            Debug.DrawLine(origin, origin + direction, Color.green);
-            freeDirection = Vector3.zero;
-            return true;
-        } else {
-            Debug.DrawLine(origin, origin + direction, Color.red);
-            freeDirection = rayDirection;
-            return false;
-        }
-    }
-
-    Vector3 ManageFieldLimit(Boid boid, Vector3 velocity) {
-        if (!goThroughtWalls) {
-            if (velocity.x < 0 && boid.transform.position.x + velocity.x < -widthOfArea / 2 || velocity.x > 0 && boid.transform.position.x + velocity.x > widthOfArea / 2) {
-                velocity.x = -velocity.x;
-            }
-            if (velocity.y < 0 && boid.transform.position.y + velocity.y < -heightOfArea / 2 || velocity.y > 0 && boid.transform.position.y + velocity.y > heightOfArea / 2) {
-                velocity.y = -velocity.y;
-            }
-        } else {
-            if (velocity.x < 0 && boid.transform.position.x + velocity.x < -widthOfArea / 2) {
-                boid.transform.position = new Vector3(widthOfArea / 2, boid.transform.position.y);
-            } else if (velocity.x > 0 && boid.transform.position.x + velocity.x > widthOfArea / 2) {
-                boid.transform.position = new Vector3(-widthOfArea / 2, boid.transform.position.y);
-            }
-            if (velocity.y < 0 && boid.transform.position.y + velocity.y < -heightOfArea / 2) {
-                boid.transform.position = new Vector3(boid.transform.position.x, heightOfArea / 2);
-            } else if (velocity.y > 0 && boid.transform.position.y + velocity.y > heightOfArea / 2) {
-                boid.transform.position = new Vector3(boid.transform.position.x, -heightOfArea / 2);
+        // Player input
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Boid b = hit.collider.gameObject.GetComponent<Boid>();
+                if (b != null)
+                    activeBoid = b;
             }
         }
-        return velocity;
-    }
+    }   
 
-    int GetCloseObjects(Boid boid, out List<Transform> boids, out List<Transform> objects) {
+    public int GetCloseObjects(Boid boid, out List<Transform> boids, out List<Transform> objects) {
         boids = new List<Transform>();
         objects = new List<Transform>();
         Collider2D[] contextColliders = Physics2D.OverlapCircleAll(boid.transform.position, boidVisionDistance);
